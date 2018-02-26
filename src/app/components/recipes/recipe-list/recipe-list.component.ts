da6865ac3ad67471e8a3c150bbf638f1903fb181
Einars Vilnis <vilniseinars@gmail.com>
2018-02-26 17:05:43 +0000

    Update

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.models';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'This is only a test','https://upload.wikimedia.org/wikipedia/commons/f/f5/Handi-chicken-recipe.jpg' )
+  ];
 
   constructor() { }
 
